3c867da73238a1d25cedbdbf880cb10b372a58da
FoltyzTheDev <foltyzcalondev@gmail.com>
2026-06-15 23:30:33 +0700

    Create User.hx

---
+++ src/sapa/User.hx
@@ -0,0 +1,7 @@
+package sapa;
+
+class User {
+  public static function sapa(nama:String, umur:Integer, alamat:String):Void {
+    trace('Halo $nama!, umur kamu $umur tahun, dan alamat kamu di $alamat :D!.');
+  }
+}
