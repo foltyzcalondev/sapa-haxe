5feb2c7f9b969ba815f2f7bca301162c0c418435
FoltyzTheDev <foltyzcalondev@gmail.com>
2026-06-15 23:24:48 +0700

    Delete src directory

--- src/haxe/sapa/User.hx
+++
@@ -1,7 +0,0 @@
-package haxe.sapa;
-
-class User {
-   public static function sapa(nama:String, umur:Int, alamat:String) {
-       trace("\x1b[1;33mHalo $nama!, umur kamu $umur tahun, dan alamat kamu di $alamat :).\x1b[0m");
-   }
-}
